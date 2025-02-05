1b3e2e3b702019166e1c16831ae3c431dd643c23
Juniors Chagas <jjuniorchagas15@gmail.com>
2025-02-05 01:33:49 -0300

    feat:+01

--- swift-learning/main.swift
+++ swift-learning/main.swift
@@ -5,6 +5,12 @@
 //  Created by Junior Chagas on 05/02/25.
 //
 
-import Foundation
+func convertIntToString(_ number: Int) -> String {
+ 
+    return String(number)
+}
+
+print(convertIntToString(1234).customMirror.subjectType)
+
 
 
